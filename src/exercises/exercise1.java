5408aa507c297337f3734e8bac2e8c3af717fe35
ryan-b-writin <ryan.bliton@gmail.com>
2020-06-29 23:14:51 -0400

    add first few files

---
+++ src/exercises/exercise1.java
@@ -0,0 +1,18 @@
+package exercises;
+import linkedlist.Node;
+
+public class exercise1 {
+	//exercise 1- write code to remove duplicates from an unsorted linked list.
+	
+	
+
+	public static void main(String[] args) {
+		Node first = new Node(1);
+		first.appendToTail(2);
+		first.appendToTail(3);
+		
+		System.out.println(first.next.next.data);
+
+	}
+
+}
---
+++ src/linkedlist/Node.java
@@ -0,0 +1,19 @@
+package linkedlist;
+
+public class Node {
+	public Node next = null;
+	public int data;
+	
+	public Node(int d){
+		data = d;
+	}
+	
+	public void appendToTail(int d){
+		Node end = new Node(d);
+		Node n = this;
+		while (n.next != null){
+			n = n.next;
+		}
+		n.next = end;
+	}
+}
