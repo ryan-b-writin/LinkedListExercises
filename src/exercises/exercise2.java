db5afd7c116007d033ed966ec48d465077e5e420
ryan-b-writin <ryan.bliton@gmail.com>
2020-07-07 22:12:37 -0400

    set up for exercise 2

---
+++ src/exercises/exercise2.java
@@ -0,0 +1,32 @@
+package exercises;
+
+import linkedlist.Node;
+
+public class exercise2 {
+	//exercise 2- return the Kth to last element of a singly linked list
+	
+	
+
+	public static void main(String[] args) {
+		Node first = new Node(1);
+		first.appendToTail(2);
+		first.appendToTail(3);
+		first.appendToTail(4);
+		first.appendToTail(5);
+		first.appendToTail(6);
+		first.appendToTail(7);
+		first.appendToTail(8);
+		first.appendToTail(9);
+		first.appendToTail(0);
+		
+		//print linked list
+		Node current = first;
+		System.out.println(first.data);
+		while(current.next != null){
+			current = current.next;
+			System.out.println(current.data);
+		}
+
+	}
+
+}
